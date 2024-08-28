03dceb8d5a7cd6485cfc80c7b9fbbb682b838781
0xjuju <jermol.jupiter@gmail.com>
2024-08-27 23:14:28 -0400

    remove unused imports

--- coinbase_api/tests/test_cb_api.py
+++ coinbase_api/tests/test_cb_api.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from unittest.mock import MagicMock
-from datetime import datetime, timedelta
 from coinbase_api.cb_api import CBApi
-import pandas as pd
 
 
 class CoinbaseClientTest(TestCase):
